0bf2ee5750ee6873fbf3fe554fc4408551812fb3
wooju <dnwnrnt@naver.com>
2022-11-07 11:35:36 +0900

    LEDBoard finish

--- LEDBoard/ViewController.swift
+++ LEDBoard/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController, LEDBoradSettingDelegate {
         }
         self.contentLabel.textColor = textColor
         self.view.backgroundColor = backgroundColor
+        // 완성
     }
 }
 
